a763f4c7d73ad948917338953cbe98aedd7de5e2
Victor Cuc <victorcuc99@yahoo.com>
2018-10-29 12:28:55 +0000

    Add camping sites

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+const campgrounds = [
+    {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
+    {name: "Granite Hill", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
+    {name: "Mountain Goats Rest", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"}
+  ];
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -8,13 +16,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/campgrounds", (req, res) => {
-  const campgrounds = [
-    {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
-    {name: "Granite Hill", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
-    {name: "Mountain Goats Rest", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"}
-  ];
-
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", (req, res) => {
+  //get data from forms and add to campgrounds array
+  const name = req.body.name;
+  const image = req.body.image;
+  const newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  //redirect to campgrounds page
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(3000, () => {
