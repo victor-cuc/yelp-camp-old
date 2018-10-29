82a7e7c9f816d90f170766d8c1a4a7bfbb8d892b
Victor Cuc <victorcuc99@yahoo.com>
2018-10-29 14:02:32 +0000

    Added grid for thumbnails and captions on campgrounds page

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 const campgrounds = [
     {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
     {name: "Granite Hill", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
-    {name: "Mountain Goats Rest", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"}
+    {name: "Mountain Goats Rest", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
+    {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
+    {name: "Granite Hill", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
+    {name: "Mountain Goats Rest", image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f0c671a3eebdbf_340.jpg"},
   ];
 
 app.use(bodyParser.urlencoded({extended:true}));
